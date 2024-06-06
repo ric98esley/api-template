41cc7534ce569c1c9f8d701eca1cdf4d480ef018
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-06-06 16:46:27 -0400

    [FETCH] (geo assets services) chage geo assets

---
+++ src/services/geo.service/geo-assets.service.js
@@ -0,0 +1,63 @@
+const { Op } = require('sequelize');
+const { models } = require('../../libs/sequelize');
+
+const mapGeo = (data) => {
+  return {
+    ip: data.id,
+    serial: data.serial,
+    alert: data.alert,
+    alertType: data.alertType,
+    latitude: data.latitude,
+    longitude: data.longitude,
+  };
+};
+
+class GeoAssets {
+  async create({ ip, serial, alert, alertType, latitude, longitude }) {
+    const geoAsset = await models.GeoAssets.create({
+      ip,
+      serial,
+      alert,
+      alertType,
+      latitude,
+      longitude,
+    });
+    return mapGeo(geoAsset);
+  }
+
+  async find({
+    ip,
+    serial,
+    alert,
+    alertType,
+    limit,
+    offset,
+    startDate,
+    endDate,
+  }) {
+    if (!isNaN(startDate)) {
+      startDate = Number(startDate);
+      endDate = Number(endDate);
+    }
+
+    const where = {};
+
+    if (ip) where.ip = { [Op.like]: `%${ip}%` };
+    if (serial) where.serial = { [Op.like]: `%${serial}%` };
+    if (alert) where.alert = alert;
+    if (alertType) where.alertType = { [Op.like]: `%${alertType}%` };
+
+    const { count, rows } = await models.GeoAssets.findAndCountAll({
+      limit: Number(limit),
+      offset: Number(offset),
+      where,
+    });
+
+    return {
+      total: count,
+      rows: rows.map((row) => mapGeo(row)),
+    };
+  }
+}
+
+module.exports = { GeoAssets };
---
+++ src/services/geo.service/index.js
@@ -0,0 +1,5 @@
+const { GeoAsset } = require("../../db/models/asset.model/geo-assets.model");
+
+module.exports = {
+  GeoAsset
+}
