4a4d4d60dc4a8c0013120c8026d20096938f61fa
Ricardo Perez <ricardo.pereze.98@gmail.com>
2024-09-03 11:06:06 -0400

    [FETCH] (Models factory)

--- src/models/index.js
+++ src/models/index.js
@@ -3,27 +3,35 @@ const assetSpecModel = require("./asset_spec.model");
 const brandModel = require("./brand.model");
 const categoryModel = require("./category.model");
 const createdByModel = require("./created_by.model");
+const customerModel = require("./customer.model");
+const geolocationModel = require("./geo.model");
 const groupModel = require("./group.model");
 const locationModel = require("./location.model");
 const locationTypeModel = require("./location_type.model");
 const maintenanceModel = require("./maintenances.model");
+const maintenanceTypeModel = require("./maintenances_type.model");
 const modelModel = require("./model.model");
 const movementsModel = require("./movements.model");
+const specificationModel = require("./specifications.model");
 const userModel = require("./user.model");
 const zoneModel = require("./zone.model");
 
 module.exports = {
-  createdByModel,
-  locationModel,
-  modelModel,
-  assetModel,
-  maintenanceModel,
   assetSpecModel,
+  assetModel,
   brandModel,
   categoryModel,
+  createdByModel,
+  customerModel,
+  geolocationModel,
   groupModel,
-  zoneModel,
   locationTypeModel,
-  userModel,
+  locationModel,
+  maintenanceTypeModel,
+  maintenanceModel,
+  modelModel,
   movementsModel,
+  specificationModel,
+  userModel,
+  zoneModel,
 }
